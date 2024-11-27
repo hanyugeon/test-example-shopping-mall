147f2c4c593a00ac675175e81ae853a8ae24a226
hanyugeon <hanyugeon@naver.com>
2024-11-27 21:24:26 +0900

    초기세팅: vitest 초기 설정 (global, jsdom, setupFiles)

--- vite.config.js
+++ vite.config.js
@@ -8,8 +8,11 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   plugins: [react(), eslint({ exclude: ['/virtual:/**', 'node_modules/**'] })],
   test: {
+    // global: true 설정 시 별다른 import 없이 vitest API 사용가능
     globals: true,
+    // jsDOM 환경에서 구동되도록 설정
     environment: 'jsdom',
+    // 테스트 실행을 위한 설정들을 setupTest.js 파일을 보고 동작하도록 설정
     setupFiles: './src/utils/test/setupTests.js',
   },
   resolve: {
